92687564c3efedb7de63361e7f6071cf5fe010c1
Jiwon Chang <hello@jwpedia.com>
2022-01-09 19:27:12 -0500

    removed unnecessary print

--- cifar-to-resnet.py
+++ cifar-to-resnet.py
@@ -23,7 +23,6 @@ def unpickle_all():
 # Given the dictionary returned from unpickle_all(), and the index of image
 # (from 0 to 59999), returns a Pillow image
 def get_pil_image(alldicts, index):
-    print(index)
     cifar_img = alldicts[index // 10000]["data"][index % 10000] # 1 x 3072 ndarray
     np_image = np.ndarray((32, 32, 3), dtype=np.uint8) # 32 x 32 x 3 ndarray
     for i in range(32):
