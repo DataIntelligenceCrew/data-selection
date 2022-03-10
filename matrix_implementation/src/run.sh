bc33ab43790031d15e7f0c4f2ece966e510c049b
Pranay Mundra <pranaymundra4510@gmail.com>
2022-03-10 18:49:59 -0500

    cleanup3.0

--- matrix_implementation/src/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python3 main.py --filepath "/localdisk3/data-selection/cifar-10-vectors-alexnet" \
-                --threshold 0.9 \
-                --number_of_partitions 10 \
-                --data_generation 0 \
-                --coverage_factor 25 \
-                --sample_weight 0.1
--- matrix_implementation/test/train.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python3 model.py --sample_weight 0.1 \
-                 --coreset 1 \
-                 --train 1
