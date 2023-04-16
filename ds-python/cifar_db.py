f890fc470ea42393a32963263796850520413275
Pranay Mundra <pranaymundra4510@gmail.com>
2023-04-15 22:42:00 -0400

    cifar_test

--- ds-python/cifar_db.py
+++ ds-python/cifar_db.py
@@ -96,99 +96,119 @@ def gfkc_db(cf, dr, dataset_name, dataset_size, num_classes):
     CC[0][list(coreset)] = 0
     GC = np.zeros((1, num_classes))
     GC[0] = np.array(dr)
-    while ((not np.all(CC[0] <= 0)) or (not np.all(GC[0]))) and len(coreset) < dataset_size:
+    while ((not np.all(CC[0] <= 0)) or (not np.all(GC[0] <= 0))) and len(coreset) < dataset_size:
         best_point, max_score = -1, float("-inf")
         possible_ids = tuple(delta.difference(coreset))
 
-        posting_list = np.zeros((len(possible_ids), dataset_size))
-        index_to_p_id = {}
-        cur.execute('select id, pl from cifar10_postinglist where id in %s', (possible_ids, ))
-        records = cur.fetchall()
+        # posting_list = np.zeros((len(possible_ids), dataset_size))
+        # index_to_p_id = {}
+        # psql_start_time = time.time()
+        # cur.execute('select id, pl from cifar10_postinglist where id in %s;', (possible_ids, ))
+        # records = cur.fetchall()
         # print('db query time: {0}'.format(time.time() - psql_start_time))
-        for index, rows in enumerate(records):
-            posting_list[index][list(rows[1])] = 1
-            index_to_p_id[index] = rows[0]
+        # for index, rows in enumerate(records):
+        #     posting_list[index][list(rows[1])] = 1
+        #     index_to_p_id[index] = rows[0]
 
-        p_scores = np.matmul(CC, posting_list.T)
-        p_scores[0] = np.array([j + np.dot(GC[0].T, labels_dict[index_to_p_id[i]]) for i, j in enumerate(p_scores[0])])
-        max_score_index = p_scores[0].argmax()
-        p_score = p_scores[0][max_score_index] 
-        if p_score > max_score:
-            max_score = p_score
-            best_point = index_to_p_id[max_score_index]
-
-        # # parallel
-        # batch_size = int(0.1 * len(possible_ids))
-        # partition_points = [tuple(possible_ids[i : i+batch_size]) for i in range(0, len(possible_ids), batch_size)]
-        # parallel_solutions = []
-        # q = multiprocessing.Queue()
-        # processes = []
-        # for i in range(len(partition_points)):
-        #     p = multiprocessing.Process(
-        #         target=best_point_finder,
-        #         args=(partition_points[i], CC, GC, labels_dict, dataset_size, q)
-        #     )
-        #     processes.append(p)
-        #     p.start()
+        # p_scores = np.matmul(CC, posting_list.T)
+        # p_scores[0] = np.array([j + np.dot(GC[0].T, labels_dict[index_to_p_id[i]]) for i, j in enumerate(p_scores[0])])
+        # max_score_index = p_scores[0].argmax()
+        # p_score = p_scores[0][max_score_index] 
+        # if p_score > max_score:
+        #     max_score = p_score
+        #     best_point = index_to_p_id[max_score_index]
         
-        # for p in processes:
-        #         sol_data = q.get()
-        #         parallel_solutions.append(sol_data)
+
+
+        # parallel
+        batch_size = int(0.1 * len(possible_ids))
+        partition_points = [tuple(possible_ids[i : i+batch_size]) for i in range(0, len(possible_ids), batch_size)]
+        parallel_solutions = []
+        q = multiprocessing.Queue()
+        processes = []
+        for i in range(len(partition_points)):
+            p = multiprocessing.Process(
+                target=best_point_finder,
+                args=(partition_points[i], CC, GC, labels_dict, dataset_size, q)
+            )
+            processes.append(p)
+            p.start()
+        
+        for p in processes:
+                sol_data = q.get()
+                parallel_solutions.append(sol_data)
             
-        # for p in processes:
-        #     p.join()
+        for p in processes:
+            p.join()
         
-        # for s in parallel_solutions:
-        #     point_id = s[0]
-        #     p_score = s[1] 
-        #     if p_score > max_score:
-        #         max_score = p_score
-        #         best_point = point_id
+        for s in parallel_solutions:
+            point_id = s[0]
+            p_score = s[1] 
+            if p_score > max_score:
+                max_score = p_score
+                best_point = point_id
 
 
         if best_point == -1:
             print("cannot find a point")
             break
         
-        # cur.execute('select pl from cifar10_postinglist where id=%s', (best_point, ))
-        # row = cur.fetchnone()[0]
-        # posting_list_best_point = np.zeros(dataset_size)
-        # posting_list_best_point[list(row)] = 1
+        cur.execute('select pl from cifar10_postinglist where id=%s;', (best_point, ))
+        row = cur.fetchall()
+        posting_list_best_point = np.zeros(dataset_size)
+        for r in row:
+            posting_list_best_point[list(r[0])] = 1
 
         coreset.add(best_point)
-        
-        CC[0] = np.clip(np.subtract(CC[0], posting_list[best_point]), 0, None)
+        print(len(coreset))
+        CC[0] = np.clip(np.subtract(CC[0], posting_list_best_point), 0, None)
         GC[0] = np.clip(np.subtract(GC[0], labels_dict[best_point]), 0, None)
 
 
     end_time = time.time()
+    if conn is not None:
+        conn.close()
     print(len(coreset))
     print('Time Taken : {0}'.format(end_time - start_time))
 
 
 
-# def best_point_finder(points, CC, GC, labels_dict, delta_size, q):
-#     conn = psycopg2.connect(database="pmundra")
-#     cur = conn.cursor()
-#     posting_list = np.zeros((len(points), delta_size))
-#     index_to_p_id = {}
-#     # psql_start_time = time.time()
-#     cur.execute('select id, pl from cifar10_postinglist where id in %s', (points, ))
-#     records = cur.fetchall()
-#     # print('db query time: {0}'.format(time.time() - psql_start_time))
+def best_point_finder(points, CC, GC, labels_dict, delta_size, q):
+    conn = psycopg2.connect(database="pmundra")
+    cur = conn.cursor()
+    posting_list = np.zeros((len(points), delta_size))
+    index_to_p_id = {}
+    psql_start_time = time.time()
+    cur.execute('select id, pl from cifar10_postinglist where id in %s;', (points, ))
+    records = cur.fetchall()
+    print('db query time: {0}'.format(time.time() - psql_start_time))
 
-#     for index, rows in enumerate(records):
-#         posting_list[index][list(rows[1])] = 1
-#         index_to_p_id[index] = rows[0]
+    for index, rows in enumerate(records):
+        posting_list[index][list(rows[1])] = 1
+        index_to_p_id[index] = rows[0]
     
-#     p_scores = np.matmul(CC, posting_list.T)
-#     max_score_index = p_scores[0].argmax()
-#     cov_score = p_scores[0][max_score_index]
-#     point_id = index_to_p_id[max_score_index]
-#     q.put((point_id, cov_score))
+    p_scores = np.matmul(CC, posting_list.T)
+    p_scores[0] = np.array([j + np.dot(GC[0].T, labels_dict[index_to_p_id[i]]) for i, j in enumerate(p_scores[0])])
+    max_score_index = p_scores[0].argmax()
+    cov_score = p_scores[0][max_score_index]
+    point_id = index_to_p_id[max_score_index]
+    q.put((point_id, cov_score))
 
-#     if conn is not None:
-#         conn.close()
+    if conn is not None:
+        conn.close()
+
+
+
+
+def test_psql():
+    conn = psycopg2.connect(database="pmundra")
+    cur = conn.cursor()
+    delta = set([i for i in range(50000)])
+    coreset = set()
+    possible_ids = tuple(delta.difference(coreset))
+    psql_time = time.time()
+    cur.execute('select id, pl from cifar10_postinglist where id in %s;', (possible_ids, ))
+    print('db_query time : {0}'.format(time.time() - psql_time))
 
 if __name__ == '__main__':
     # posting_list_to_db()
@@ -212,3 +232,4 @@ if __name__ == '__main__':
     
     dr = [params.distribution_req] * params.num_classes
     gfkc_db(params.coverage_factor, dr, params.dataset, params.dataset_size, params.num_classes)
+    # test_psql()
